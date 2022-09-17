e2089f0f405790805a1c854e35cc31949d495673
Andrew Angelle <andrewangelle@gmail.com>
2022-09-17 17:13:29 -0400

    useRect

---
+++ src/useRect/observeRect.ts
@@ -0,0 +1,97 @@
+export type PartialRect = Partial<DOMRect>;
+
+type RectProps = {
+  rect: DOMRect | undefined;
+  hasRectChanged: boolean;
+  callbacks: Function[];
+};
+
+const props: Array<keyof DOMRect> = [
+  'bottom',
+  'height',
+  'left',
+  'right',
+  'top',
+  'width',
+];
+
+function rectChanged(
+  // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+  a: DOMRect = {} as DOMRect,
+  // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+  b: DOMRect = {} as DOMRect
+): boolean {
+  return props.some(prop => a[prop] !== b[prop]);
+}
+
+const observedNodes = new Map<Element, RectProps>();
+
+let rafId: number;
+
+function run(): void {
+  const changedStates: RectProps[] = [];
+
+  observedNodes.forEach((state, node) => {
+    const newRect = node.getBoundingClientRect();
+    if (rectChanged(newRect, state.rect)) {
+      state.rect = newRect;
+      changedStates.push(state);
+    }
+  });
+
+  changedStates.forEach(state => {
+    state.callbacks.forEach(cb => cb(state.rect));
+  });
+
+  rafId = window.requestAnimationFrame(run);
+}
+
+export function observeRect(
+  node: Element,
+  callback: (rect: DOMRect) => void
+): {
+  observe: () => void;
+  unobserve: () => void;
+} {
+  return {
+    observe() {
+      const wasEmpty = observedNodes.size === 0;
+
+      if (observedNodes.has(node)) {
+        observedNodes.get(node)?.callbacks.push(callback);
+      } else {
+        observedNodes.set(node, {
+          rect: undefined,
+          hasRectChanged: false,
+          callbacks: [callback],
+        });
+      }
+
+      if (wasEmpty) {
+        run();
+      }
+    },
+
+    unobserve() {
+      const state = observedNodes.get(node);
+
+      if (state != null && state !== undefined) {
+        const index = state.callbacks.indexOf(callback);
+
+        if (index >= 0) {
+          state.callbacks.splice(index, 1);
+        }
+
+        // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+        if (!state.callbacks.length) {
+          observedNodes.delete(node);
+        }
+
+        // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+        if (!observedNodes.size) {
+          cancelAnimationFrame(rafId);
+        }
+      }
+    },
+  };
+}
---
+++ src/useRect/useRect.ts
@@ -0,0 +1,80 @@
+import { useLayoutEffect, useState, useRef, RefObject } from 'react';
+
+import { observeRect, PartialRect } from './observeRect';
+
+export type UseRectOptions = {
+  observe?: boolean;
+  onChange?: (rect: PartialRect) => void;
+};
+
+export function useRect<ElementType extends Element = HTMLElement>(
+  nodeRef: RefObject<ElementType | undefined | null>,
+  options?: UseRectOptions
+): null | DOMRect {
+  let observe: boolean;
+  let onChange: UseRectOptions['onChange'];
+
+  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+  if (options?.observe) {
+    observe = options.observe;
+  } else {
+    observe = true;
+    onChange = options?.onChange;
+  }
+
+  const [element, setElement] = useState(nodeRef.current);
+  const initialRectIsSet = useRef<boolean>(false);
+  const initialRefIsSet = useRef<boolean>(false);
+  const [rect, setRect] = useState<DOMRect | null>(null);
+  const onChangeRef = useRef<UseRectOptions['onChange']>(onChange);
+
+  useLayoutEffect(() => {
+    onChangeRef.current = onChange;
+
+    if (nodeRef.current !== element) {
+      setElement(nodeRef.current);
+    }
+  });
+
+  useLayoutEffect(() => {
+    if (
+      element !== null &&
+      element !== undefined &&
+      !initialRectIsSet.current
+    ) {
+      initialRectIsSet.current = true;
+      setRect(element.getBoundingClientRect());
+    }
+  }, [element]);
+
+  useLayoutEffect(() => {
+    if (!observe) {
+      return;
+    }
+
+    let elem = element;
+
+    if (!initialRefIsSet.current) {
+      initialRefIsSet.current = true;
+      elem = nodeRef.current;
+    }
+
+    if (elem === null || elem === undefined) {
+      // eslint-disable-next-line no-useless-return
+      return;
+    }
+
+    const observer = observeRect(elem, rect => {
+      onChangeRef.current?.(rect);
+      setRect(rect);
+    });
+
+    observer.observe();
+
+    return () => {
+      observer.unobserve();
+    };
+  }, [observe, element, nodeRef]);
+
+  return rect;
+}
