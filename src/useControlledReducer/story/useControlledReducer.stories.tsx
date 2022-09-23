6bd9b5ec46e8ecbf2d3b52663707d6cf9f60da75
Andrew Angelle <andrewangelle@gmail.com>
2022-09-23 15:32:19 -0400

    cleanup

--- src/useControlledReducer/story/useControlledReducer.stories.tsx
+++ src/useControlledReducer/story/useControlledReducer.stories.tsx
@@ -1,120 +1,100 @@
-import { SelectState, selectActionTypes } from "./reducers";
-import { useSelect } from "./useSelect";
+import { SelectState, selectActionTypes } from './reducers';
+import { useSelect } from './useSelect';
 
 const Meta = {
   title: 'useControlledReducer',
-  component: () => null
-}
+  component: () => null,
+};
 
 const selectOptions = [
-  {value: '1', label: 'option 1'},
-  {value: '2', label: 'option 2'},
-  {value: '3', label: 'option 3'}
-]
+  { value: '1', label: 'option 1' },
+  { value: '2', label: 'option 2' },
+  { value: '3', label: 'option 3' },
+];
 
-export function Basic(){
-  const { 
-    isOpen, 
-    selectedOption, 
-    toggle, 
-    setSelectedOption 
-  } = useSelect({ options: selectOptions });
-
-  return (
-    <div>
-      <div 
-        style={{cursor: 'pointer'}}
-        onClick={toggle}
-      >
-        {selectedOption?.label || 'placeholder'}
-      </div>
-      
-      {isOpen && (
-        <div 
-          style={{
-            display: 'flex', 
-            flexDirection: 'column', 
-            border: '1px solid black',
-            cursor: 'pointer'
-          }}
-        >
-          {selectOptions.map(option => (
-            <div 
-              key={option.value} 
-              onClick={() => setSelectedOption(option)}
-            >
-              {option.label}
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  )
-}
-
-export function WithPropOverrides(){
-  const overriddenDisplayedValue = {
-    label: 'overriden displayed value', 
-    value: '0'
-  }
-
-  const { 
-    isOpen, 
-    selectedOption, 
-    toggle, 
-    setSelectedOption 
-  } = useSelect({ 
+export function Basic() {
+  const { isOpen, selectedOption, toggle, setSelectedOption } = useSelect({
     options: selectOptions,
-    selectedOption: overriddenDisplayedValue,
-    stateReducer(state, action){
-      let changes: Partial<SelectState> = {...action.changes || {}}
-
-      switch(action.type){
-        case selectActionTypes.setSelectedOption:
-          changes = {
-            ...changes,
-            isOpen: false
-          }
-        break;
-      }
-      
-      return {
-        ...state,
-        ...changes
-      }
-    }
   });
 
   return (
     <div>
-      <div 
-        style={{cursor: 'pointer'}}
-        onClick={toggle}
-      >
+      <div style={{ cursor: 'pointer' }} onClick={toggle}>
         {selectedOption?.label || 'placeholder'}
       </div>
-      
+
       {isOpen && (
-        <div 
+        <div
           style={{
-            display: 'flex', 
-            flexDirection: 'column', 
+            display: 'flex',
+            flexDirection: 'column',
             border: '1px solid black',
-            cursor: 'pointer'
+            cursor: 'pointer',
           }}
         >
           {selectOptions.map(option => (
-            <div 
-              key={option.value} 
-              onClick={() => setSelectedOption(option)}
-            >
+            <div key={option.value} onClick={() => setSelectedOption(option)}>
               {option.label}
             </div>
           ))}
         </div>
       )}
     </div>
-  )
+  );
 }
 
-export default Meta
+export function WithPropOverrides() {
+  const overriddenDisplayedValue = {
+    label: 'overriden displayed value',
+    value: '0',
+  };
+
+  const { isOpen, selectedOption, toggle, setSelectedOption } = useSelect({
+    options: selectOptions,
+    selectedOption: overriddenDisplayedValue,
+    stateReducer(state, action) {
+      let changes: Partial<SelectState> = { ...(action.changes || {}) };
+
+      switch (action.type) {
+        case selectActionTypes.setSelectedOption:
+          changes = {
+            ...changes,
+            isOpen: false,
+          };
+          break;
+      }
+
+      return {
+        ...state,
+        ...changes,
+      };
+    },
+  });
+
+  return (
+    <div>
+      <div style={{ cursor: 'pointer' }} onClick={toggle}>
+        {selectedOption?.label || 'placeholder'}
+      </div>
+
+      {isOpen && (
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            border: '1px solid black',
+            cursor: 'pointer',
+          }}
+        >
+          {selectOptions.map(option => (
+            <div key={option.value} onClick={() => setSelectedOption(option)}>
+              {option.label}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Meta;
